1a38d4d3d6fcbb0bbcf2147d61e9954ec17309fc
arief <ariefshaik001@gmail.com>
2025-09-23 23:28:14 +0530

    helm

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:5001/api' });
+// Change the baseURL to a relative path.
+// The Ingress will now handle routing the request to the correct service.
+const api = axios.create({ baseURL: '/api' });
+
 api.interceptors.request.use(config => {
   const token = sessionStorage.getItem('token');
   if (token) { config.headers['x-access-token'] = token; }
   return config;
 }, error => Promise.reject(error));
+
 api.interceptors.response.use(response => response, error => {
     if (error.response && error.response.status === 401) {
         sessionStorage.removeItem('token');
@@ -12,4 +16,5 @@ api.interceptors.response.use(response => response, error => {
     }
     return Promise.reject(error);
 });
+
 export default api;
