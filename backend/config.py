d97025f9342d9d6f994baff5a23d93c4c59c9275
arief <ariefshaik001@gmail.com>
2025-09-23 18:58:18 +0530

    argocd

--- backend/config.py
+++ backend/config.py
@@ -4,3 +4,4 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-key-change-in-prod")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///simplenotes.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
