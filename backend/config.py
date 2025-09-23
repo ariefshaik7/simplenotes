917a3fb5c56d6158ff6cb668240dc04bc0ee85b0
arief <ariefshaik001@gmail.com>
2025-09-24 00:51:55 +0530

    simplenotes

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-key-change-in-prod")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///simplenotes.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
